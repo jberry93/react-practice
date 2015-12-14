ca9bf5c2094d9ceb812522c4db7c01ac38c5e55d
Jeffrey Berry <jeffreyberry93@gmail.com>
2015-12-14 14:15:08 -0800

    Created a simple component

---
+++ componentsProperties/app.js
@@ -0,0 +1,9 @@
+var ProgrammingLanguage = React.createClass({
+  render: function() {
+    return (
+      <h3>I want to learn JavaScript</h3>
+    );
+  }
+});
+
+ReactDOM.render(<ProgrammingLanguage />, document.getElementById("content"));
