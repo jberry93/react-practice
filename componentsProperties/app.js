7725143ec8e28dd823549f0b70ec288b14a4af7d
Jeffrey Berry <jeffreyberry93@gmail.com>
2015-12-14 15:13:38 -0800

    Our final result JavaScript for the componentProperties section

--- componentsProperties/app.js
+++ componentsProperties/app.js
@@ -1,9 +1,16 @@
 var ProgrammingLanguage = React.createClass({
   render: function() {
     return (
-      <h3>I want to learn JavaScript</h3>
+      <h3>{this.props.person} wants to learn {this.props.language}</h3>
     );
   }
 });
 
-ReactDOM.render(<ProgrammingLanguage />, document.getElementById("content"));
+ReactDOM.render(
+  <div>
+    <ProgrammingLanguage person="Jim" language="JavaScript" />
+    <ProgrammingLanguage person="Betty" language="Python" />
+    <ProgrammingLanguage person="George" language="Ruby" />
+    <ProgrammingLanguage person="Jane" language="PHP" />
+    <ProgrammingLanguage person="Jeff" language="C++" />
+  </div>, document.getElementById("content"));
