95fff443ac1561a66b16c0170f76e9a1dea77f37
Jeffrey Berry <jeffreyberry93@gmail.com>
2016-01-14 17:24:50 -0800

    Added an event listener to the browserify task

--- gulpPerfection/gulpfile.js
+++ gulpPerfection/gulpfile.js
@@ -8,14 +8,20 @@ var gulp = require("gulp"),
 
 gulp.task("browserify", function() {
   var bundlify = browserify({
-    entries: [],
-    transform: [reactify],
+    entries: ["insert/file/here"], // whatever file goes here will have its dependencies included
+    transform: [reactify], // JSX => JS
     debug: true,
-    // need these next 3 properties to enable 'watchify'
+    // need these next 3 properties are required by 'watchify'
     cache: {}, 
     packageCache: {},
     fullPaths: true
   });
 
   var stalkify = watchify(bundlify);
+
+  return stalkify.on("update", function() {
+    var currentTime = Date.now();
+    console.log("Currently updating!");
+    stalkify.bundle().pipe(source("insert/fileName/from/entries/here")).pipe(gulp.dest("insert/file/destination/here"));
+  });
 });
