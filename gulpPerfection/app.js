43eebdf82a289d196c7dd96b9eee625017035723
Jeffrey Berry <jeffreyberry93@gmail.com>
2016-01-15 16:29:42 -0800

    Added a React component to render onto the html file

---
+++ gulpPerfection/app.js
@@ -0,0 +1,10 @@
+var React = require("react"),
+    ReactDOM = require("react-dom");
+
+var HelloWorld = React.createClass({
+  render: function() {
+    return (<h1>Hello World!!!</h1>);
+  }
+});
+
+ReactDOM.render(<HelloWorld />, document.getElementById("helloWorld"));
